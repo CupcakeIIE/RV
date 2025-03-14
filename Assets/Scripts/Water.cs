e72e632d6df3643e8102bee8edf068e259892b37
Orio44 <arnaud@marquis.bzh>
2025-03-14 12:19:08 +0100

    script pour remplir la casserole

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Water : MonoBehaviour
 {
     [SerializeField] private GameObject _water;
+    [SerializeField] private GameObject _waterInPot;
+    private bool _isFull;
+
+    private void Start()
+    {
+        _isFull = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject == _water) enabled = true;
+        if(other.gameObject == _water && !_isFull)
+        {
+            GameObject fullWater = Instantiate(_waterInPot, transform);
+            fullWater.transform.SetLocalPositionAndRotation(new Vector3(0f, 0.35f, 0f), Quaternion.identity);
+            _isFull = true;
+        }
     }
 }
