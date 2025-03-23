e28f3fa922e341c0a35c24b4dcc4001b64139d2b
Pierrick360 <82902651+Pierrick360@users.noreply.github.com>
2025-03-23 16:34:41 +0100

    wtf sound

--- Assets/Scripts/CookingFood.cs
+++ Assets/Scripts/CookingFood.cs
@@ -37,12 +37,12 @@ public class CookingFood : MonoBehaviour
                 counter += 1;
             }
 
-            if (counter >= 500)
+            /*if (counter >= 500)
             {
                 this.transform.GetComponent<Renderer>().material.color = new Color(0.64f, 0.164f, 0.164f, 1.0f);;
-            }
+            }*/
 
-            if (counter >= 1000)
+            if (counter >= 100)
             {
                 counter = 0;
                 this.transform.GetComponent<Renderer>().material.color = Color.black;
