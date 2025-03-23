5e7d2f5da3e2bac540948061ac52a5b08597fc3e
Pierrick360 <82902651+Pierrick360@users.noreply.github.com>
2025-03-23 15:57:46 +0100

    encofre plus de sound giga stylax

--- Assets/Scripts/Sink.cs
+++ Assets/Scripts/Sink.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Sink : MonoBehaviour
 {
     private bool _isOpened;
+    private AudioSource audioSource;
     [SerializeField] private GameObject _water;
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();  
         _isOpened = false;
     }
 
     public void Open()
     {
         _isOpened = !_isOpened;
+        if (_isOpened){
+            audioSource.Play();
+        } else {
+            audioSource.Stop();
+        }
         _water.SetActive(_isOpened);
     }
 }
